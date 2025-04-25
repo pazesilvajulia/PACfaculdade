f2242157f3885be25ac5ee2f00b9862129f0d703
Julia Paz <117188894+pazesilvajulia@users.noreply.github.com>
2025-04-24 21:40:34 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 
-fetch('components/header.html')
+fetch('/components/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-container').innerHTML = data;
   });
+
+fetch('/components/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-container').innerHTML = data;
+  });
+
+
 
 function toggleMenu() {
   const menu = document.getElementById('menu');
